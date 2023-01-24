fe0fb1c8a7663f918c11c393c54b1762f3b06820
uzulla <uzulla@himitsukichi.com>
2023-01-24 18:25:00 +0900

    Wow. I'm newbie...

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # link_brew_and_phpenv
+
+link brew's php to `phpenv/versions/brew`.
+
+## usage
+
+```
+$ curl -s "https://raw.githubusercontent.com/uzulla/link_brew_and_phpenv/main/link.sh" | bash
+```
+
--- link.sh
+++ link.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-brew -h 2&>1 > /dev/null || exit 1
-phpenv -h 2&>1 > /dev/null || exit 1
+brew -h > /dev/null 2>&1 || exit 1
+phpenv -h > /dev/null 2>&1 || exit 1
 
 ls -d `brew --prefix`/Cellar/php/* | xargs -I {} -L 1 ln -sf {} `which phpenv | sed "s/\/bin\/phpenv//g"`/versions/brew && phpenv rehash
 
