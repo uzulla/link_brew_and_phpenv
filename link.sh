58a9dbdc8989a0e17c8e864bd66a6a47fdbd7240
uzulla <uzulla@himitsukichi.com>
2023-01-24 18:22:29 +0900

    chore

--- link.sh
+++ link.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+brew -h 2&>1 > /dev/null || exit 1
+phpenv -h 2&>1 > /dev/null || exit 1
+
 ls -d `brew --prefix`/Cellar/php/* | xargs -I {} -L 1 ln -sf {} `which phpenv | sed "s/\/bin\/phpenv//g"`/versions/brew && phpenv rehash
 
 
