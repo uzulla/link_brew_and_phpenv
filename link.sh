8bd2de508305f46cf7c242df6b6723076c3d3622
uzulla <uzulla@himitsukichi.com>
2023-01-24 18:10:59 +0900

    fix first time.

--- link.sh
+++ link.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-ls -d `brew --prefix`/Cellar/php/* | xargs -I {} -L 1 ln -sf {} `phpenv which php | sed "s/\/bin\/php//g"` && phpenv rehash
+ls -d `brew --prefix`/Cellar/php/* | xargs -I {} -L 1 ln -sf {} `which phpenv | sed "s/\/bin\/phpenv//g"` && phpenv rehash
+
 
